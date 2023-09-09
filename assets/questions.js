f76e9a37d2b0350aba9d84ec1af7d106738f7d96
itsbbea <bea.may27@hotmail.com>
2023-09-09 13:51:14 +1000

    updated script.js file and renamed it to questions.js, added questions to questions.js file, added new script.js file.

---
+++ assets/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["booleans", "strings", "alerts", "numbers"]
+
+    },
+
+    {
+        question: "The condition in an if / else statement is encolsed within ________.",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "curly brackets"
+
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+
+    {
+        question: "String values must be enlosed within ________ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+
+]
